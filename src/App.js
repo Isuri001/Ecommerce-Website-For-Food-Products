f796f97bd6b5881d809e0869479e9283eec8ba7b
Isuri001 <isuriupeksha258@gmail.com>
2024-01-11 20:29:21 +0530

    Create header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
-   <div className='bg-red-500'>
-   React app
+   <div >
+      <Header/>
    </div>
   );
 }

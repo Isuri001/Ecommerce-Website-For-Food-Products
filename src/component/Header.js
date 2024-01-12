e1f8f154e29acc4ee29eefaa189e484ee959f394
Isuri001 <isuriupeksha258@gmail.com>
2024-01-12 08:06:39 +0530

    Create login,newproduct pages in page folder and make routes for it

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,44 +1,63 @@
-import React from 'react'
-import logo from "../assest/logo.png"
+import React, { useState } from "react";
+import logo from "../assest/logo.png";
 import { Link } from "react-router-dom";
-import {FaUserAlt} from "react-icons/fa"
+import { FaUserCircle } from "react-icons/fa";
 import { MdShoppingCart } from "react-icons/md";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleShowMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
   return (
-    <header className='fixed shadow-md w-full h-16 px-2 md:px-4 z-50'>
+    <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50">
+      {/* desktop */}
 
-        {/* desktop */}
- 
-        <div className='flex items-center h-full justify-between'>
-            <Link to={""}>
-                <div className='h-10'>
-                  <img src={logo} className='h-full '></img>
-                </div>
-            </Link>
+      <div className="flex items-center h-full justify-between">
+        <Link to={""}>
+          <div className="h-10">
+            <img src={logo} className="h-full "></img>
+          </div>
+        </Link>
 
-            <div className="flex items-center gap-4 md:gap-7">
-              <nav className='flex gap-4 md:gap-6 text-base md:text-lg'>
-                <Link to={""}>Home</Link>
-                <Link to={"menu"}>Menu</Link>
-                <Link to={"about"}>About</Link>
-                <Link to={"contact"}>Contact</Link>
-              </nav>
-              <div className='text-2xl text-slate-600 relative'>
-                <MdShoppingCart />
-                <div className='absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm  text-center'>
-                  0
-                </div>
-              </div>
-              <div className='text-2xl text-slate-600'>
-                <FaUserAlt/>
-              </div>
+        <div className="flex items-center gap-4 md:gap-7">
+          <nav className="flex gap-4 md:gap-6 text-base md:text-lg">
+            <Link to={""}>Home</Link>
+            <Link to={"menu"}>Menu</Link>
+            <Link to={"about"}>About</Link>
+            <Link to={"contact"}>Contact</Link>
+          </nav>
+          <div className="text-2xl text-slate-600 relative">
+            <MdShoppingCart />
+            <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm  text-center">
+              0
             </div>
+          </div>
+          <div className=" text-slate-600" onClick={handleShowMenu}>
+            <div className="text-2xl cursor-pointer">
+              <FaUserCircle />
+            </div>
+
+            {showMenu && (
+              <div className="absolute right-2 bg-white py-2 px-2 shadow drop-shadow-md flex flex-col">
+                <Link
+                  to={"newproduct"}
+                  className="whitespace-nowrap cursor-pointer"
+                >
+                  New Product
+                </Link>
+                <Link to={"login"} className="whitespace-nowrap cursor-pointer">
+                  Login
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
+      </div>
 
-        {/* mobile */}
+      {/* mobile */}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

1d589226a2ddb91d1c29f4e019bcc8dcb1f343cf
Isuri001 <isuriupeksha258@gmail.com>
2024-01-19 11:49:12 +0530

    Home Page Frontend

---
+++ src/component/HomeCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const HomeCard = () => {
+  // return <div className="bg-slate-100 rounded"></div>;
+  // return (
+  //   // <div className="bg-white shadow-md p-2 rounded">
+  //   //   <div className="w-40 min-h-[140px]">
+  //   //     <img src={image} className="w-full h-full pt-2 " />
+  //   //   </div>
+  //   //   <h3 className="font-semibold text-slate-600 text-center capitalize text-lg ">
+  //   //     {name}
+  //   //   </h3>
+  //   //   <p className="text-center text-slate-500 font-medium">{category}</p>
+  //   //   <p className="text-center font-bold">
+  //   //     <span className="text-red-500 font-semibold">Rs.</span>
+  //   //     <span>{price}</span>
+  //   //   </p>
+  //   // </div>
+  // );
+  return <img src={require("../assest/grocery.png")} className=""></img>;
+};
+
+export default HomeCard;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,7 +1,61 @@
 import React from "react";
+import HomeCart from "../component/HomeCard";
+import HomeCard from "../component/HomeCard";
+import { useSelector } from "react-redux";
 
 const Home = () => {
-  return <div>Home</div>;
+  const productData = useSelector((state) => state.product.productList);
+  console.log(productData);
+  const homeProductCartList = productData.slice(0, 4);
+
+  return (
+    <div className="p-2 md:p-4">
+      <div className="md:flex gap-3 py-0">
+        <div className="md:w-1/2">
+          <div className="flex gap-3 bg-slate-300 w-36 px-2 items-center rounded-full">
+            <p className="text-sm font-medium text-slate-900">Bike Delivery</p>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2972/2972185.png "
+              className="h-7"
+            ></img>
+          </div>
+          <h2 className="text-4xl py-4 md:text-7xl font-bold">
+            The Fasted Delivery in{" "}
+            <span className="text-green-900">Your Home</span>
+          </h2>
+          <p className="py-5 text-base">
+            Welcome to{" "}
+            <span className="font-medium text-green-500">Food Franzy </span> !
+            Discover a world where passion meets flavor, and quality meets
+            convenience. Our e-commerce platform is crafted for food enthusiasts
+            like you. Start a unique gastronomic journey with our handpicked
+            delights, guaranteed to elevate your dining experience.
+          </p>
+          <button className="font-bold bg-green-400 text-slate-200 px-6 py-3 rounded-md">
+            Order Now
+          </button>
+        </div>
+        {/* <div className="md:w-1/2 flex flex-wrap gap-4 p-4 justify-center">
+          {homeProductCartList[0] &&
+            homeProductCartList.map((el) => {
+              return (
+                <HomeCard
+                  key={el._id}
+                  image={el.image}
+                  name={el.name}
+                  price={el.price}
+                  category={el.category}
+                />
+              );
+            })}
+        </div> */}
+
+        <div className="md:w-1/2 flex p-0 justify-center">
+          <HomeCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

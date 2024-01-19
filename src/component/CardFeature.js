09812ce8338f5be3005543efab32dc885d901beb
Isuri001 <isuriupeksha258@gmail.com>
2024-01-19 16:04:09 +0530

    Vegetables section done

---
+++ src/component/CardFeature.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CardFeature = ({ image, name, price, category }) => {
+  return (
+    <div className="w-full min-w-[200px] bg-white cursor-pointer hover:drop-shadow-lg pt-5 px-4 rounded flex flex-col">
+      <div className="h-28 flex flex-col justify-center items-center">
+        <img src={image} className="h-full" />
+      </div>
+      <h3 className="font-semibold text-slate-600  capitalize text-md mt-4 whitespace-nowrap overflow-hidden">
+        {name}
+      </h3>
+      <p className=" text-slate-500 font-sm">{category}</p>
+      <p className=" font-bold">
+        <span className="text-red-500 font-semibold">Rs.</span>
+        <span className="font-semibold">{price}</span>
+      </p>
+      <button className="bg-green-400 rounded py-1 my-4 ">Add to cart</button>
+    </div>
+  );
+};
+
+export default CardFeature;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import HomeCart from "../component/HomeCard";
 import HomeCard from "../component/HomeCard";
 import { useSelector } from "react-redux";
+import CardFeature from "../component/CardFeature";
+import { GrNext, GrPrevious } from "react-icons/gr";
 
 const Home = () => {
   const productData = useSelector((state) => state.product.productList);
   console.log(productData);
   const homeProductCartList = productData.slice(0, 4);
+  const homeProductCartListVegetables = productData.filter(
+    (el) => el.category === "vegetables",
+    []
+  );
+  console.log(homeProductCartListVegetables);
+
+  const loadingArray = new Array(4).fill(null);
+
+  const slideProductRef = useRef();
+  const nextProduct = () => {
+    slideProductRef.current.scrollLeft += 200;
+  };
+  const preveProduct = () => {
+    slideProductRef.current.scrollLeft -= 200;
+  };
 
   return (
     <div className="p-2 md:p-4">
@@ -53,6 +70,44 @@ const Home = () => {
         <div className="md:w-1/2 flex p-0 justify-center">
           <HomeCard />
         </div>
+      </div>
+
+      <div className=" ">
+        <div className="flex w-full items-center">
+          <h2 className="font-bold text-2xl text-slate-800 mb-4">
+            Fresh Vegetables
+          </h2>
+          <div className="ml-auto flex gap-4">
+            <button
+              onClick={preveProduct}
+              className="bg-slate-300 hover:bg-slate-400 text-md p-1 rounded"
+            >
+              <GrPrevious />
+            </button>
+            <button
+              onClick={nextProduct}
+              className="bg-slate-300 hover:bg-slate-400 text-md p-1 rounded"
+            >
+              <GrNext />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className="flex gap-5 overflow-scroll scrollbar-none scroll-smooth transition-all"
+        ref={slideProductRef}
+      >
+        {homeProductCartListVegetables.map((el) => {
+          return (
+            <CardFeature
+              key={el._id}
+              name={el.name}
+              category={el.category}
+              price={el.price}
+              image={el.image}
+            />
+          );
+        })}
       </div>
     </div>
   );
